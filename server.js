ebb02020305dfe4ae901d39c2ab8f38e0ba0ef14
ChapalBarua <chapalbuet@yahoo.com>
2023-12-09 01:14:13 -0500

    [Temple-7] Serving the backend using https instead of http

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const fs = require("fs");
 const PORT = 3000;
 const app = express();
 const bodyParser = require("body-parser");
 const appRoutes = require('./routes');
+var https = require('https');
 var cors = require("cors");
 const mongooseConnection = require('./helpers/mongoose-connection');
+
+var options = {
+    key: fs.readFileSync('certs/cert.key'),
+    cert: fs.readFileSync('certs/cert.crt')
+  };
 
 function logger(req, res, next){
     console.log(`[${Date.now()}] ${req.method} ${req.url}`);
@@ -28,4 +35,4 @@ app.use(logger);
 // });
 
 mongooseConnection();
-app.listen(PORT,()=>console.log(`Server is now listening on port ${PORT}`));
+https.createServer(options, app).listen(PORT,()=>console.log(`Server is now listening on port ${PORT}`));
