c07204de2d8dd7b4dfc01ead285eeab7d9801543
ChapalBarua <chapalbuet@yahoo.com>
2024-01-06 15:43:18 -0500

    [temple-21] change directory address for EC2

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
-    UPLOADED_FILE_PATH: 'C:/Users/chapa/Desktop/temple_uploaded/temple_uploaded_files/',
-    UPLOADED_IMAGE_PATH: 'C:/Users/chapa/Desktop/temple_uploaded/temple_uploaded_images/',
-    SERVED_DIRECTORY: 'C:/Users/chapa/Desktop/temple_uploaded',
-    DOWNLOADABLE_FILE_URL: 'https://localhost:3000/temple_uploaded_files/',
-    DOWNLOADABLE_IMAGE_URL: 'https://localhost:3000/temple_uploaded_images/'
+    UPLOADED_FILE_PATH: '/home/ec2-user/temple_uploaded/temple_uploaded_files/',
+    UPLOADED_IMAGE_PATH: '/home/ec2-user/temple_uploaded/temple_uploaded_images/',
+    SERVED_DIRECTORY: '/home/ec2-user/temple_uploaded',
+    DOWNLOADABLE_FILE_URL: 'https://ec2-18-212-240-65.compute-1.amazonaws.com:3000/temple_uploaded_files/',
+    DOWNLOADABLE_IMAGE_URL: 'https://ec2-18-212-240-65.compute-1.amazonaws.com:3000/temple_uploaded_images/'
 });
