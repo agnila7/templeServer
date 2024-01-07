0fd6d842f2372b1374579aaf573e87b769a79b7e
EC2 Default User <ec2-user@ip-172-31-17-109.ec2.internal>
2024-01-07 09:10:51 +0000

    [temple-26] Run UI successfully with node

---
+++ site.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const PORT = 8088;
+var https = require('https');
+const path = require('path');
+var fs = require('fs');
+
+
+var options = {
+    key: fs.readFileSync('/home/ec2-user/templeServer/certs/cert.key'),
+    cert: fs.readFileSync('/home/ec2-user/templeServer/certs/cert.crt')
+  };
+
+
+
+
+const app = express();
+app.use(express.static(__dirname + '/dist/TempleWebsite'));
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+https.createServer(options, app).listen(PORT,()=>console.log(`Server is now listening on port ${PORT}`));
