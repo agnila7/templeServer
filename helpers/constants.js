04727b328859ade0c7ffe05cd288d11da8a80f74
ChapalBarua <chapalbuet@yahoo.com>
2024-01-08 21:29:57 -0500

    [temple-27] ue dns name

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,6 +2,6 @@ module.exports = Object.freeze({
     UPLOADED_FILE_PATH: '/home/ec2-user/temple_uploaded/temple_uploaded_files/',
     UPLOADED_IMAGE_PATH: '/home/ec2-user/temple_uploaded/temple_uploaded_images/',
     SERVED_DIRECTORY: '/home/ec2-user/temple_uploaded',
-    DOWNLOADABLE_FILE_URL: 'https://ec2-18-212-240-65.compute-1.amazonaws.com:3000/temple_uploaded_files/',
-    DOWNLOADABLE_IMAGE_URL: 'https://ec2-18-212-240-65.compute-1.amazonaws.com:3000/temple_uploaded_images/'
+    DOWNLOADABLE_FILE_URL: 'https://www.bso-toronto.ca:3000/temple_uploaded_files/',
+    DOWNLOADABLE_IMAGE_URL: 'https://www.bso-toronto.ca:3000/temple_uploaded_images/'
 });
